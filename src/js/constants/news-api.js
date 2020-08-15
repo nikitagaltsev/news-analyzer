17ba0c23d32073917aa71527ed4269d038091f5f
nikitagaltsev <nikitagalcev@gmail.com>
2020-08-15 16:25:45 +0300

    babel fix

--- src/js/constants/news-api.js
+++ src/js/constants/news-api.js
@@ -1,7 +1,7 @@
 import NewsApi from '../modules/NewsApi';
 
 const newsApi = new NewsApi({
-  baseUrl: 'https://newsapi.org/',
+  baseUrl: ' https://nomoreparties.co/news',
   headers: {
     authorization: '0934753f1d2b453d88b568743d71b5fe',
     'Content-Type': 'application/json'
